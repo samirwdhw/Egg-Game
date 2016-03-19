35d500fd0562f10338cae8dd2e39995683335bac
Samir <samir.wdhw@gmail.com>
2016-03-19 22:15:30 +0530

    Egg catches 1 egg

--- Egg_Game.py
+++ Egg_Game.py
@@ -13,10 +13,7 @@ fpsClock = pygame.time.Clock()
 #Initialize displaysurf
 DISPLAYSURF	= pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), 0, 32)
 
-
-
-
-
+#Duck Things
 duck_w = 600/10
 duck_h = 540/10
 duckImg = pygame.image.load('duck.png')
@@ -24,16 +21,55 @@ duckImg = pygame.transform.scale(duckImg, (int(duck_w), int(duck_h)))
 movingRight = False
 movingLeft = False
 
-
 #Co-ordinates of the duck
+
 duck_x = 0
 duck_y = 400
+
+#To store all the eggs
+eggs = []
+egg_w = 589/25
+egg_h = 800/25
+
+#Egg things
+
+class Egg(object):
+
+	def __init__ (self, x, y):
+		self.w = 589/25
+		self.h = 800/25
+		self.Img = pygame.image.load('egg.png')
+		self.Img = pygame.transform.scale(self.Img, (self.w, self.h))
+		self.x = x
+		self.y = y
+
+	def move(self):
+		self.y += MOTIONSPEED
+
+	def destroy(self):
+		eggs.remove(self)
+
+eggs.append(Egg(random.randint(duck_x - 50, duck_x + 50),egg_h))
 
 
 while True: 
 
 	DISPLAYSURF.fill(BGCOLOR)
 	DISPLAYSURF.blit(duckImg, (duck_x, duck_y))
+
+
+
+	for egg in eggs:
+		
+		if( egg.x in range(duck_x, duck_x + duck_w) and egg.y in range(duck_y, duck_y + duck_h)):
+			egg.destroy()
+
+		elif( egg.y >= duck_h + duck_y):
+			egg.destroy()
+
+		else:
+			DISPLAYSURF.blit(egg.Img, (egg.x, egg.y))
+			egg.move()
 
 	for event in pygame.event.get():
 		
