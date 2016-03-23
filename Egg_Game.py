c9e81846ee8d921238f19316af909665e2046a6a
Samir <samir.wdhw@gmail.com>
2016-03-23 18:46:11 +0530

    Start screen template set

--- Egg_Game.py
+++ Egg_Game.py
@@ -15,6 +15,84 @@ BGCOLOR = (102,205,170)
 MOTIONSPEED = 10   #Speed of the moving duck
 fpsClock = pygame.time.Clock()
 
+#to render the text
+def render_textrect(string, font, rect, text_color, background_color, justification=0):
+    """Returns a surface containing the passed text string, reformatted
+    to fit within the given rect, word-wrapping as necessary. The text
+    will be anti-aliased.
+
+    Takes the following arguments:
+
+    string - the text you wish to render. \n begins a new line.
+    font - a Font object
+    rect - a rectstyle giving the size of the surface requested.
+    text_color - a three-byte tuple of the rgb value of the
+                 text color. ex (0, 0, 0) = BLACK
+    background_color - a three-byte tuple of the rgb value of the surface.
+    justification - 0 (default) left-justified
+                    1 horizontally centered
+                    2 right-justified
+
+    Returns the following values:
+
+    Success - a surface object with the text rendered onto it.
+    Failure - raises a TextRectException if the text won't fit onto the surface.
+    """
+
+    import pygame
+    
+    final_lines = []
+
+    requested_lines = string.splitlines()
+
+    # Create a series of lines that will fit on the provided
+    # rectangle.
+
+    for requested_line in requested_lines:
+        if font.size(requested_line)[0] > rect.width:
+            words = requested_line.split(' ')
+            # if any of our words are too long to fit, return.
+            for word in words:
+                if font.size(word)[0] >= rect.width:
+                    raise TextRectException, "The word " + word + " is too long to fit in the rect passed."
+            # Start a new line
+            accumulated_line = ""
+            for word in words:
+                test_line = accumulated_line + word + " "
+                # Build the line while the words fit.    
+                if font.size(test_line)[0] < rect.width:
+                    accumulated_line = test_line 
+                else: 
+                    final_lines.append(accumulated_line) 
+                    accumulated_line = word + " " 
+            final_lines.append(accumulated_line)
+        else: 
+            final_lines.append(requested_line) 
+
+    # Let's try to write the text out on the surface.
+
+    surface = pygame.Surface(rect.size) 
+    surface.fill(background_color) 
+
+    accumulated_height = 0 
+    for line in final_lines: 
+        if accumulated_height + font.size(line)[1] >= rect.height:
+            raise TextRectException, "Once word-wrapped, the text string was too tall to fit in the rect."
+        if line != "":
+            tempsurface = font.render(line, 1, text_color)
+            if justification == 0:
+                surface.blit(tempsurface, (0, accumulated_height))
+            elif justification == 1:
+                surface.blit(tempsurface, ((rect.width - tempsurface.get_width()) / 2, accumulated_height))
+            elif justification == 2:
+                surface.blit(tempsurface, (rect.width - tempsurface.get_width(), accumulated_height))
+            else:
+                raise TextRectException, "Invalid justification argument: " + str(justification)
+        accumulated_height += font.size(line)[1]
+
+    return surface
+
+
 #To keep score
 SCORE = 0
 
@@ -66,6 +144,7 @@ egg_h = 800/25
 
 #To store broken eggs
 omlets = []
+
 
 class Omlet(object):
 
@@ -144,6 +223,38 @@ class Egg(object):
 	def destroy(self):
 		eggs.remove(self)
 
+#Things to do when the game starts
+
+def startScreen():
+
+	global myfont
+
+	BGCOLOR = (220,20,60)
+
+	welcome_string = "Welcome to the game\n Hope You enjoy it\n Click any button to continue"
+	story_string = "One day mummy duck was walking down the road. Soon the weather turned bad and it started raining. But, it wasn't water, Her eggs were falling!!!"
+	controls_string = "-> : Move right\n<- : Move Left\nSPACEBAR: Jump\n(Hint: Dont let the chicks get away)"
+	
+	welcome = myfont.render(welcome_string, 1, (0,0,128))
+
+ 	my_rect = pygame.Rect((40, 40, 300, 300))
+ 	rendered_text = render_textrect(welcome_string, myfont, my_rect, (216, 216, 216), (48, 48, 48), 0)
+
+	DISPLAYSURF.fill(BGCOLOR)
+	DISPLAYSURF.blit(rendered_text, (WINDOWWIDTH/2, WINDOWHEIGHT/2))
+	
+	pygame.display.update()
+	
+	event = pygame.event.wait()
+
+	while event.type != KEYDOWN:
+		event = pygame.event.wait()
+
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+
 
 #Things after the game is over
 
@@ -171,7 +282,7 @@ def gameOverScreen():
 	pygame.display.update()
 
 	pygame.event.wait()
-	pygame.event.wait()
+	#pygame.event.wait()
 	pygame.quit()
 	sys.exit()
 
@@ -181,6 +292,8 @@ def jump():
 
 	movingUp = True;
 
+
+startScreen()
 
 while True: 
 
